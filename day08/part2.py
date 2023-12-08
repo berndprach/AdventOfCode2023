9af2696e1e2265ce704d86fbc9e0923ff23157a0
Bernd Prach <bprach@live.com>
2023-12-08 11:44:53 +0100

    Added day 8.

--- day08/part2.py
+++ day08/part2.py
@@ -53,26 +53,28 @@ def find_loop(starting_node, next_node, instructions):
                 step = iteration * len(instructions) + i + 1
                 end_node_indices.append(step)
 
+    raise ValueError("Did not find loop in 1000 iterations")
+
 
 def print_end_node_equations(starting_node, next_node, instructions,
                              with_simplification=False):
     print(f"\nConsidering starting node {starting_node}.")
-    loop_start_step, loop_end_step, goal_node_indices = find_loop(
+    loop_start_step, loop_end_step, end_node_indices = find_loop(
         starting_node, next_node, instructions
     )
     loop_steps = loop_end_step - loop_start_step
-    for goal_node_index in goal_node_indices:
-        if goal_node_index < loop_start_step:
-            print(f"   Single solution at step {goal_node_index}")
+    for end_node_index in end_node_indices:
+        if end_node_index < loop_start_step:
+            print(f"   Single solution at step {end_node_index}")
         else:
             print(f"   Repeating solution at "
-                  f"{goal_node_index} + i * {loop_steps}")
+                  f"{end_node_index} + i * {loop_steps}")
 
     if with_simplification:
-        loop_goal_node_indices = [i for i in goal_node_indices
-                                  if i >= loop_start_step]
-        goal_index, loop_steps = simplify(loop_goal_node_indices, loop_steps)
-        print(f"   Simplified to: {goal_index} + i * {loop_steps}")
+        loop_end_node_indices = [i for i in end_node_indices
+                                 if i >= loop_start_step]
+        end_index, loop_steps = simplify(loop_end_node_indices, loop_steps)
+        print(f"   Simplified to: {end_index} + i * {loop_steps}")
 
 
 def print_all_end_node_equations(lines: list[str], with_simplification=False):
@@ -85,27 +87,27 @@ def print_all_end_node_equations(lines: list[str], with_simplification=False):
 
 
 # Attempt 2b: Simplify equations:
-def simplify(goal_indices, loop_steps):
-    if len(goal_indices) == 1:
-        return goal_indices[0], loop_steps
+def simplify(end_indices, loop_steps):
+    if len(end_indices) == 1:
+        return end_indices[0], loop_steps
 
-    min_index = min(goal_indices)
-    new_goals = [min_index]
-    for i in goal_indices:
+    min_index = min(end_indices)
+    new_ends = [min_index]
+    for i in end_indices:
         if (i - min_index) % loop_steps == 0:
             continue
-        new_goals.append(i)
+        new_ends.append(i)
 
-    if len(new_goals) == 1:
-        return new_goals[0], loop_steps
+    if len(new_ends) == 1:
+        return new_ends[0], loop_steps
 
-    length = len(new_goals)
+    length = len(new_ends)
     regular_indices = [i * min_index for i in range(1, length + 1)]
-    if loop_steps % length == 0 and new_goals == regular_indices:
+    if loop_steps % length == 0 and new_ends == regular_indices:
         return min_index, loop_steps // length
 
     raise Exception(f"Unclear how to simplify "
-                    f"{goal_indices} and {loop_steps}")
+                    f"{end_indices} and {loop_steps}")
 
 
 # Attempt 2c: Final solution:
@@ -115,12 +117,12 @@ def solve(lines: list[str]) -> int:
 
     all_loop_steps = []
     for node in starting_nodes:
-        loop_start_step, loop_end_step, goal_node_indices = find_loop(
+        loop_start_step, loop_end_step, end_node_indices = find_loop(
             node, next_node, instructions
         )
         loop_steps = loop_end_step - loop_start_step
-        goal_index, loop_steps = simplify(goal_node_indices, loop_steps)
-        assert goal_index == loop_steps
+        end_index, loop_steps = simplify(end_node_indices, loop_steps)
+        assert end_index == loop_steps
 
         all_loop_steps.append(loop_steps)
 
