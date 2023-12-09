fd32c83366db919cd0af8cf7b02505faf7ec7aea
bprach <bprach@ist.ac.at>
2023-12-09 13:36:04 +0100

    Added day 9.

---
+++ day09/part1.py
@@ -0,0 +1,39 @@
+import numpy as np
+
+
+def read_input() -> list[str]:
+    with open("input.txt") as f:
+        lines = f.read().splitlines()
+    return lines
+
+
+def parse_line(line: str) -> list[int]:
+    # "0 3 6 9 12 15"
+    return [int(v) for v in line.split(" ")]
+
+
+def predict_next_number(numbers: np.array) -> int:
+    if all(n == 0 for n in numbers):
+        return 0
+
+    differences = numbers[1:] - numbers[:-1]
+    new_difference = predict_next_number(differences)
+    return numbers[-1] + new_difference
+
+
+def solve(lines: list[str]) -> int:
+    solutions = 0
+    for line in lines:
+        numbers = parse_line(line)
+        solutions += predict_next_number(np.array(numbers))
+    return solutions
+
+
+def main():
+    lines = read_input()
+    solution = solve(lines)
+    print(f"{solution = }")
+
+
+if __name__ == "__main__":
+    main()
---
+++ day09/part2.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+from part1 import read_input, parse_line, predict_next_number
+
+
+def solve(lines: list[str]) -> int:
+    solutions = 0
+    for line in lines:
+        numbers = parse_line(line)
+        reverse_numbers = numbers[::-1]
+        previous_number = predict_next_number(np.array(reverse_numbers))
+        solutions += previous_number
+    return solutions
+
+
+def main():
+    lines = read_input()
+    solution = solve(lines)
+    print(f"{solution = }")
+
+
+if __name__ == "__main__":
+    main()
