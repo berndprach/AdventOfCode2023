89cf649b485f1ce10d41cae0320602c417a7bc56
Bernd Prach <bprach@live.com>
2023-12-10 21:08:23 +0100

    Added day 10.

--- day10/part1.py
+++ day10/part1.py
@@ -16,11 +16,8 @@ class Position:
     def __add__(self, other):
         return Position(self.x + other.x, self.y + other.y)
 
-    def __neg__(self):
-        return Position(-self.x, -self.y)
-
     def __sub__(self, other):
-        return self + (-other)
+        return Position(self.x - other.x, self.y - other.y)
 
     def __eq__(self, other):
         return self.x == other.x and self.y == other.y
