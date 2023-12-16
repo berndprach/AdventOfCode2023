9bb467dd31e5c57f14a68be051d32b865c1d4a1e
Bernd Prach <bprach@live.com>
2023-12-16 15:56:00 +0100

    Added day 16.

--- day16/part1.py
+++ day16/part1.py
@@ -1,26 +1,14 @@
 from enum import Enum
 
-
-class Position:
-    def __init__(self, x: int, y: int):
-        self.x = x
-        self.y = y
-
-    def __add__(self, other):
-        return Position(self.x + other.x, self.y + other.y)
-
-    def __hash__(self):
-        return hash((self.x, self.y))
-
-    def __eq__(self, other):
-        return self.x == other.x and self.y == other.y
+Position = tuple[int, int]
+Grid = dict[Position, str]
 
 
 class Direction(Enum):
-    North = Position(0, -1)
-    East = Position(1, 0)
-    South = Position(0, 1)
-    West = Position(-1, 0)
+    North = (0, -1)
+    East = (1, 0)
+    South = (0, 1)
+    West = (-1, 0)
 
 
 def get_new_directions(current_direction: Direction,
@@ -81,7 +69,10 @@ def find_number_of_enegized_positions(grid: dict[Position, str],
         is_energized[position] = True
         new_directions = get_new_directions(direction, grid[position])
         for new_direction in new_directions:
-            new_position = position + new_direction.value
+            new_position = (
+                position[0] + new_direction.value[0],
+                position[1] + new_direction.value[1]
+            )
             if new_position in grid:
                 active_light_beams.append((new_position, new_direction))
 
@@ -98,15 +89,17 @@ def parse_input(lines: list[str]) -> dict[Position, str]:
     grid = {}
     for y, line in enumerate(lines):
         for x, symbol in enumerate(line):
-            grid[Position(x, y)] = symbol
+            grid[(x, y)] = symbol
     return grid
 
 
 def solve(lines: list[str]) -> int:
     grid = parse_input(lines)
-    initial_position = Position(0, 0)
+    initial_position = (0, 0)
     initial_direction = Direction.East
-    return find_number_of_enegized_positions(grid, initial_position, initial_direction)
+    return find_number_of_enegized_positions(
+        grid, initial_position, initial_direction
+    )
 
 
 def main():
--- day16/part2.py
+++ day16/part2.py
@@ -11,16 +11,16 @@ from part1 import (
 def solve(lines: list[str]) -> int:
     grid = parse_input(lines)
 
-    max_x = max(position.x for position in grid.keys())
-    max_y = max(position.y for position in grid.keys())
+    max_x = max(x for x, y in grid.keys())
+    max_y = max(y for x, y in grid.keys())
 
     starting_configurations = []
     for x in range(max_x + 1):  # Coming from top or bottom
-        starting_configurations.append((Position(x, 0), Direction.South))
-        starting_configurations.append((Position(x, max_y), Direction.North))
+        starting_configurations.append(((x, 0), Direction.South))
+        starting_configurations.append(((x, max_y), Direction.North))
     for y in range(max_y + 1):  # Coming from left or right
-        starting_configurations.append((Position(0, y), Direction.East))
-        starting_configurations.append((Position(max_x, y), Direction.West))
+        starting_configurations.append(((0, y), Direction.East))
+        starting_configurations.append(((max_x, y), Direction.West))
 
     max_enegized = 0
     for starting_position, starting_direction in starting_configurations:
